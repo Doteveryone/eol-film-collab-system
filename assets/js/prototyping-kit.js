b21c33f21c01698fde2be0dc02c98c4ed4cee71c
ntlk <hi@ntlk.net>
2016-06-14 10:22:28 +0100

    Remove merge artifacts

--- assets/js/prototyping-kit.js
+++ assets/js/prototyping-kit.js
@@ -539,10 +539,7 @@ var App = Backbone.Model.extend({
   },
 
   showHomeScreen: function() {
-<<<<<<< HEAD
     var homeScreen = this.findScreen('home');
-=======
->>>>>>> master
     if (typeof homeScreen === 'undefined') {
       console.log('No home screen has been set. Add \'data-screen="home"\' to the HTML element you want to be show first to fix this error')
     } else {
